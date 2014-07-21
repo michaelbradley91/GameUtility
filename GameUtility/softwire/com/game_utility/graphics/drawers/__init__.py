df1e966774bf05b44cc1f0f5c37c4222deaaa492
michael <michael.bradley@hotmail.co.uk>
2014-07-21 16:01:36 +0100

    Deregistration fix - forgot to update screen

---
+++ GameUtility/softwire/com/game_utility/graphics/drawers/__init__.py
@@ -0,0 +1,7 @@
+'''
+This package is what it is all about!!! At long last..
+
+This package contains all of the objects which draw things to the screen.
+It is strongly suggested that you use these objects to draw as they will register
+to the picture handler properly for you.
+'''
